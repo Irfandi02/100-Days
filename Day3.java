414b32fe22a244478358cd27f022168987333cf3
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-11 19:46:46 +0800

    Day3
    Belajar tipe data primitif

---
+++ Day3.java
@@ -0,0 +1,19 @@
+package codin;
+
+public class Day3 {
+
+  public static void main(String[] args) {
+   
+   // melengkapi codingan tipe data kemarin di hari 2 :
+   
+ //  tipe data primitif
+     byte umur = 18;
+     long nomorwa = 81543214753L;
+     short tahun = 2023;
+     
+     System.out.println("umur ="+umur);
+     System.out.println("nomor wa ="+nomorwa);
+     System.out.println("tahun ="+tahun);
+     
+   
+  }
