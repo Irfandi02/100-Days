b69bb2eae522c9d556dcfc23ba9da2efef965caf
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-12 21:03:27 +0800

    Day.4

---
+++ Day4.java
@@ -0,0 +1,39 @@
+public class Day4 {
+
+  public static void main(String[] args) {
+  
+    // disini kita akan membahas nilai maksimum tipe data:
+    // integer,byte,short
+    
+    // integer
+    int i = 10;
+    System.out.println("=====INTEGER=====");
+    System.out.println("nilai integer =" + 1);
+    System.out.println("nilai max =" + Integer.MAX_VALUE);
+    System.out.println("nilai min =" + Integer.MIN_VALUE);
+    System.out.println("Besar integer =" + Integer.BYTES +"bytes");
+    System.out.println("Besar integer =" + Integer.SIZE +"bit");
+    
+    // byte
+    byte b = 10;
+    System.out.println("=====BYTE====");
+    System.out.println("nilai byte =" + 1);
+    System.out.println("nilai max =" + Byte.MAX_VALUE);
+    System.out.println("nilai min =" + Byte.MIN_VALUE);
+    System.out.println("Besar byte =" + Byte.BYTES +"bytes");
+    System.out.println("Besar byte =" + Byte.SIZE +"bit");
+    
+    // short
+    short s = 10;
+    System.out.println("=====SHORT=====");
+    System.out.println("nilai short =" + 1);
+    System.out.println("nilai max =" + Short.MAX_VALUE);
+    System.out.println("nilai min =" + Short.MIN_VALUE);
+    System.out.println("Besar short =" + Short.BYTES +"bytes");
+    System.out.println("Besar short =" + Short.SIZE +"bit");
+    
+    
+         
+    }
+    
+}
