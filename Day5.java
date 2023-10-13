f9e4c5960f8f2ae2da5e570fba2655ae8e56b5aa
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-13 21:29:16 +0800

    Day5
    Mempelajari operasi aritmatika

---
+++ Day5.java
@@ -0,0 +1,35 @@
+public class Day5 {
+
+  public static void main(String[] args) {
+  
+    // operasi aritmatika
+    
+    int angka1 = 6;
+    int angka2 = 4;
+     
+     int hasil;
+     
+    // penjumlahan
+      hasil = angka1 + angka2;
+      System.out.println(angka1 + "+" + angka2 + "=" + hasil);
+    
+    
+    // pengurangan
+      hasil = angka1 - angka2;
+      System.out.printf("%d - %d = %d /n",angka1,angka2,hasil);
+      
+     // perkalian
+      hasil = angka1 * angka2;
+      System.out.printf("%d * %d = %d /n",angka1,angka2,hasil);
+      
+      // pembagian
+       hasil = angka1 / angka2;
+       System.out.printf("%d / %d = %d /n",angka1,angka2,hasil);
+      
+      // modulus
+       hasil = angka1 % angka2;
+       System.out.printf("%d %% %d =%d /n",angka1,angka2,hasil);
+         
+    }
+    
+}
