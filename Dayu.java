141f328b7452868207d748b16db2efc3c195caa8
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-15 19:40:10 +0800

    Day7
    Belajar penugasan operator penugasan

---
+++ Dayu.java
@@ -0,0 +1,11 @@
+public class Day7 {
+
+  public static void main(String[] args) {
+   
+   // operator penugasan
+    int nilai = 11;
+    System.out.println(nilai);
+        nilai += 2;
+     System.out.println(nilai);
+  }
+}
