5550408296912ddae0d2be74e81c4447c2b41bc8
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-18 20:20:18 +0800

    Day10
    Mempelajari operator pembanding

---
+++ Day10.java
@@ -0,0 +1,36 @@
+package Day10;
+
+public class Day10 {
+
+  public static void main(String[] args) {
+   
+   //kali ini kita akan membahas operator perbandingan
+   
+   int a = 10;
+   int b = 20;
+   boolean hasil;
+   
+   hasil = a == b;
+   System.out.println("apakah a == b ?" + hasil);
+   
+   hasil = a != b;
+   System.out.println("apakah a != b ?" + hasil);
+   
+   hasil = a > b;
+   System.out.println("apakah a > b ?" + hasil);
+   
+   hasil = a < b;
+   System.out.println("apakah a < b ?" + hasil);
+   
+   hasil = a >= b;
+   System.out.println("apakah a >= b ?" + hasil);
+   
+   hasil = a <= b ;
+   System.out.println("apakah a <= b ?" + hasil);
+   
+   
+   
+   
+  }
+}
+ 
