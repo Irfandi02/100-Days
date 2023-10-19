006d094f4a8666051920381b4ec2449311358e8f
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-19 19:29:53 +0800

    Operator logika
    Membuat operator logika

---
+++ Operator logika.java
@@ -0,0 +1,23 @@
+package Day11;
+
+public class Day11 {
+
+  public static void main(String[] args) {
+ 
+   // kita akan membuat operator logika
+   
+   boolean a = true ;
+   boolean b = false ;
+   boolean hasil;
+   
+   hasil = a && b;
+   System.out.println("hasil dari a && b :" + hasil);
+   
+   hasil = a || b;
+   System.out.println("hasil dari a || b :" + hasil);
+   
+   hasil = !a;
+   System.out.println("hasil dari !b :" + hasil);
+   
+  }
+}
