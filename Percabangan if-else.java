a97947f0376f1dab4c0556db60223185ed9c401b
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-10-20 19:46:58 +0800

    Percabangan if-else
    Belajar percabangan if- else

---
+++ Percabangan if-else.java
@@ -0,0 +1,18 @@
+package percabangan;
+
+public class percabangan {
+
+  public static void main(String[] args) {
+   
+   // hari ini kita akan membahas percabangan if-else
+
+   if (10 > 20)
+     System.out.println("sangat bagus");
+     
+   else {
+     System.out.println("santuy");
+       
+   }
+   
+  }
+}
