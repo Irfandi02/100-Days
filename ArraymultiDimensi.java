37c3e6f2455ce6f69057b39e546785b082709f6f
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-11-23 22:30:25 +0800

    ArraymultiDimensi
    Membuat program menggunakan array multidimensi

---
+++ ArraymultiDimensi.java
@@ -0,0 +1,17 @@
+package ArraymultiDimensi;
+
+public class ArraymultiDimensi {
+
+  public static void main(String[] args) {
+  
+  // penggunaan array multidimensi
+  String [][] buah = new String[2][2];
+     buah[0][0] = "semangka";
+     buah[0][1] = "anggur";
+     buah[1][0] = "durian";
+     buah[1][1] = "strawberry";
+     
+     System.out.print(buah[0][1]);
+   
+  }
+}
