9c24f65b8650591631547f510bf523b923ad3e0d
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-11-28 21:43:41 +0800

    Arrberurutan
    Membuat program array berurutan menggunakan bahasa pemrograman Java

---
+++ Arrberurutan.java
@@ -0,0 +1,18 @@
+package isc;
+
+import java.util.Arrays;
+
+public class isc {
+
+  public static void main(String[] args) {
+  
+  int[] ArrayInteger1 = {55,43,51,68,80,25};
+  int[] ArrayInteger2 = {30,53,25,75,39,90};
+  
+  Arrays.sort(ArrayInteger1);
+  System.out.println(Arrays.toString(ArrayInteger1));
+  Arrays.sort(ArrayInteger2);
+  System.out.println(Arrays.toString(ArrayInteger2));
+   
+  }
+}
