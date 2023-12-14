bf982ea21b64b1908962340a74cbde501f6106d0
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-12-14 21:49:44 +0800

    faktorial.
    Membuat program sederhana yang menampilkan faktorial angka

---
+++ faktorial.java
@@ -0,0 +1,21 @@
+package fktorial;
+
+import java.util.Scanner;
+
+public class fktorial {
+
+  public static void main(String[] args) {
+  
+  Scanner input = new Scanner(System.in);
+  System.out.print("Masukkan bilangan :");
+  int a = input.nextInt();
+  System.out.print("Faktorisasi Prima "+ a +" Adalah :");
+  for (int i = 2; i <= a; i++){
+    while(a % i == 0 ){
+      System.out.print(i +" ");
+      a /= i;
+    }
+  }
+  System.out.println(" ");
+  }
+}
