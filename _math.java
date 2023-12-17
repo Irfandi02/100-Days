5b3ec1e4c3b1f2becb5040022a841ab8b2dab7f4
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-12-17 22:34:28 +0800

    _math
    Membuat program sederhana dengan math

---
+++ _math.java
@@ -0,0 +1,15 @@
+package day70;
+public class day70 {
+  public static void main(String[] args) {
+  
+  long x = 76540000;
+  long y = 9500000;
+  long a = Math.min(x, y);
+  long b = Math.max(x, y);
+  
+     System.out.println("Nilai Minimal "+a);
+     System.out.println("Nilai Maksimal "+b);
+  
+   
+  }
+}
