146a3d4aea681d757f829f7501b15239fb32899b
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-12-20 22:00:16 +0800

    73_Mathabs
    Membuat program sederhana yang menampilkan nilai absolut suatu bilangan

---
+++ 73_Mathabs.java
@@ -0,0 +1,20 @@
+package day73;
+
+import java.util.Scanner;
+
+public class day73 {
+
+  public static void main(String[] args) {
+  
+  Scanner input = new Scanner(System.in);
+  
+  System.out.print("masukkan nilai :");
+  int a = input.nextInt();
+  
+  int b = Math.abs(a);
+  System.out.println("Nilai absolut dari a adalah = "+b);
+  
+  
+   
+  }
+}
