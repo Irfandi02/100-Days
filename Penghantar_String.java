d21c30f23630a5b03ad83f649bbbaa6e32a531f9
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2023-12-31 21:16:56 +0800

    Penghantar_String
    Membuat program penghantar string pada java

---
+++ Penghantar_String.java
@@ -0,0 +1,14 @@
+package pengantar_string;
+
+  public class pengantarstring {
+  
+    public static void main(String[] args){
+      String nama = "Irfandi";
+      System.out.println("jml karakter\t: "+nama.length());
+      System.out.println("uppercase\t: "+nama.toUpperCase());
+      System.out.println("lowercase\t: "+nama.toLowerCase());
+      
+      System.out.println("char[0]\t\t:"+nama.charAt(0));
+      
+    } 
+  }
