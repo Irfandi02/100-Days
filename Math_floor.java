65618f1c8123104faefef9ac80e808cef5c619d6
Irfandi02 <146800223+Irfandi02@users.noreply.github.com>
2024-01-04 19:57:24 +0800

    Math_floor
    Membuat program sederhana menggunakan fungsi math.floor untuk melakukan pembulatan ke bawah

---
+++ Math_floor.java
@@ -0,0 +1,11 @@
+package Math_floor;
+
+public class MathFloor {
+
+    public static void main(String[] args) {
+        double angka = 6.8;
+        double hasilFloor = Math.floor(angka);
+
+        System.out.println("Hasil floor dari " + angka + " adalah: " + hasilFloor);
+    }
+}
